f76db49cff4b27ee35125c112e453cbe17c70ca8
John <jsuchocki@gmail.com>
2016-09-13 15:41:49 -0400

    challenge 2 done

--- challenge_2/challenge2.js
+++ challenge_2/challenge2.js
@@ -1,5 +1,28 @@
 $(function(){
+	
+	var colorToggle = true;
+	
+	// 1 - works initial time only, not fully functional
+	$("h1").on('click', function() {
+		if(colorToggle)
+			$("h1").css('color', 'red');
+		else {
+			$("h1").css('color', 'black');
+		}
+		colorToggle = !colorToggle;
+	});
 
-	//all your code should go here
+	//2 - done
+	$("h5:odd").hide();
 
+	//3 - done
+	$("p").not("aside p").text().replace("Bacon", "LASER VISION");
+
+	//4 - done
+	$(".post:last-of-type").remove();
+	$(".post:last-of-type").remove();
+	
+	//5 - done
+	$("aside img").remove();
+	
 })
